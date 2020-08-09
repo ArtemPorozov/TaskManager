faff5fdfcd51943e272951cde407ce5c7293a63d
Artem <aporozov90@gmail.com>
2020-08-09 15:31:25 +0300

    BaseTaskController is added, TaskController and TaskForSelectedDayController were refactored

--- TaskManager/Controllers/List of Tasks/Task for Selected Day/TaskForSelectedDayController.swift
+++ TaskManager/Controllers/List of Tasks/Task for Selected Day/TaskForSelectedDayController.swift
@@ -9,50 +9,20 @@
 import UIKit
 import RealmSwift
 
-class TaskForSelectedDayController: UIViewController, UITextFieldDelegate, SubtasksListForDayTaskDelegate {
+final class TaskForSelectedDayController: BaseTaskController, SubtasksListForDayTaskDelegate {
 
     // MARK: - Private Properties
 
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.backgroundColor = .white
-        scrollView.alwaysBounceVertical = true
-        scrollView.isScrollEnabled = true
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.keyboardDismissMode = .interactive
-        return scrollView
-    }()
-    
-    private let tasksTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Task"
-        label.font = .boldSystemFont(ofSize: 42)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    private let subtasksTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Subtasks completed today:"
-        label.font = .boldSystemFont(ofSize: 30)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let subtasksList = SubtasksListForDayTask()
-    
-    private var task: Task?
     private var taskForDay: TaskForDay?
+    private let subtasksListForDayTask = SubtasksListForDayTask()
     private var subtasksListHeightConstraint: NSLayoutConstraint?
-    private var scrollViewBottomAnchor: NSLayoutConstraint?
-
     
     // MARK: - Initializers
     
     init(task: TaskForDay) {
         self.taskForDay = task
-        self.subtasksList.taskForDay = task
-        self.subtasksList.subtasks = task.subtasks
+        self.subtasksListForDayTask.taskForDay = task
+        self.subtasksListForDayTask.subtasks = task.subtasks
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -67,22 +37,16 @@ class TaskForSelectedDayController: UIViewController, UITextFieldDelegate, Subta
         
         setupNavigationBar()
         setupViews()
-        setupKeyboardObservers()
         // dismissing the keyboard by tapping the screen
         setupGesture()
-        subtasksList.delegate = self
+        subtasksListForDayTask.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
-        let contentHeight = 24 + tasksTitleLabel.frame.height + 48 + subtasksTitleLabel.frame.height + 16 + subtasksList.collectionView.contentSize.height + 48
+        let contentHeight = 24 + tasksTitleLabel.frame.height + 48 + subtasksTitleLabel.frame.height + 16 + subtasksListForDayTask.collectionView.contentSize.height + 48
         scrollView.contentSize = CGSize(width: view.frame.width, height: contentHeight)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: - Private Methods
@@ -92,74 +56,21 @@ class TaskForSelectedDayController: UIViewController, UITextFieldDelegate, Subta
     }
     
     private func setupViews() {
-        
-        view.backgroundColor = .white
-
-        view.addSubview(scrollView)
-        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        
-        scrollViewBottomAnchor = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        scrollViewBottomAnchor?.isActive = true
-        
-        scrollView.addSubview(tasksTitleLabel)
-        tasksTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
-        tasksTitleLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16).isActive = true
-        tasksTitleLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16).isActive = true
-        tasksTitleLabel.heightAnchor.constraint(equalToConstant: 48).isActive = true
 
         tasksTitleLabel.text = taskForDay?.name
+        
+        subtasksTitleLabel.text = "Subtasks completed today:"
 
-        scrollView.addSubview(subtasksTitleLabel)
-        subtasksTitleLabel.topAnchor.constraint(equalTo: tasksTitleLabel.bottomAnchor, constant: 48).isActive = true
-        subtasksTitleLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16).isActive = true
-        subtasksTitleLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16).isActive = true
-        subtasksTitleLabel.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        scrollView.addSubview(subtasksListForDayTask.view)
+        addChild(subtasksListForDayTask)
+        subtasksListForDayTask.view.translatesAutoresizingMaskIntoConstraints = false
+        subtasksListForDayTask.view.topAnchor.constraint(equalTo: subtasksTitleLabel.bottomAnchor, constant: 16).isActive = true
+        subtasksListForDayTask.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        subtasksListForDayTask.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
 
-        scrollView.addSubview(subtasksList.view)
-        addChild(subtasksList)
-        subtasksList.view.translatesAutoresizingMaskIntoConstraints = false
-        subtasksList.view.topAnchor.constraint(equalTo: subtasksTitleLabel.bottomAnchor, constant: 16).isActive = true
-        subtasksList.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        subtasksList.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-                
         setSubtasksListHeightConstraint()
     }
-    
-    private func setupKeyboardObservers() {
 
-        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc private func handleKeyboardWillShow(notification: NSNotification) {
-        
-        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-        guard let keyboardDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
-        
-        scrollViewBottomAnchor?.constant = -keyboardFrame.height
-        
-        var contentInset: UIEdgeInsets = self.scrollView.contentInset
-        contentInset.bottom = 24
-        scrollView.contentInset = contentInset
-        
-        UIView.animate(withDuration: keyboardDuration) {
-            self.view.layoutIfNeeded()
-        }
-    }
-    
-    @objc private func handleKeyboardWillHide(notification: NSNotification) {
-        
-        guard let keyboardDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
-
-        scrollViewBottomAnchor?.constant = -48
-        UIView.animate(withDuration: keyboardDuration) {
-            self.view.layoutIfNeeded()
-        }
-    }
-    
     private func setupGesture() {
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
         tap.cancelsTouchesInView = false
@@ -167,20 +78,8 @@ class TaskForSelectedDayController: UIViewController, UITextFieldDelegate, Subta
     }
     
     private func setSubtasksListHeightConstraint() {
-        subtasksListHeightConstraint = subtasksList.view.heightAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(subtasksList.numItems ?? 0) * subtasksList.itemHeight)
+        subtasksListHeightConstraint = subtasksListForDayTask.view.heightAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(subtasksListForDayTask.numItems ?? 0) * subtasksListForDayTask.itemHeight)
         subtasksListHeightConstraint?.isActive = true
-    }
-    
-    // MARK: - Text Field Delegate
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        if textField.text! == "" {
-            return false
-        } else {
-            textField.resignFirstResponder()
-            return true
-        }
     }
     
     // MARK: - Subtasks List For Day Task Delegate
--- TaskManager/Controllers/List of Tasks/Task/TaskController.swift
+++ TaskManager/Controllers/List of Tasks/Task/TaskController.swift
@@ -9,28 +9,14 @@
 import UIKit
 import RealmSwift
 
-final class TaskController: UIViewController, UITextFieldDelegate {
+final class TaskController: BaseTaskController {
     
+    // MARK: - Public Properties
+    
+    let subtasksList = SubtasksList()
+        
     // MARK: - Private Properties
 
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.backgroundColor = .white
-        scrollView.alwaysBounceVertical = true
-        scrollView.isScrollEnabled = true
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.keyboardDismissMode = .interactive
-        return scrollView
-    }()
-    
-    private let tasksTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Task"
-        label.font = .boldSystemFont(ofSize: 42)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
     private let textField: UITextField = {
         let textField = UITextField()
         textField.textAlignment = .left
@@ -38,19 +24,9 @@ final class TaskController: UIViewController, UITextFieldDelegate {
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
-    
-    private let subtasksTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Subtasks"
-        label.font = .boldSystemFont(ofSize: 30)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
     private var task: Task?
-    private let subtasksList = SubtasksList()
-    private var scrollViewBottomAnchor: NSLayoutConstraint?
-        
+    private var subtasksListHeightConstraint: NSLayoutConstraint?
+
     // MARK: - Initializers
     
     init(task: Task) {
@@ -70,7 +46,6 @@ final class TaskController: UIViewController, UITextFieldDelegate {
 
         setupNavigationBar()
         setupViews()
-        setupKeyboardObservers()
         // dismissing the keyboard by tapping the screen
         setupGesture()
         
@@ -83,11 +58,6 @@ final class TaskController: UIViewController, UITextFieldDelegate {
 
         let contentHeight = 24 + tasksTitleLabel.frame.height + 24 + subtasksTitleLabel.frame.height + 16 + subtasksList.collectionView.contentSize.height + 48
         scrollView.contentSize = CGSize(width: view.frame.width, height: contentHeight)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: - Private Methods
@@ -142,22 +112,6 @@ final class TaskController: UIViewController, UITextFieldDelegate {
     }
     
     private func setupViews() {
-                
-        view.backgroundColor = .white
-
-        view.addSubview(scrollView)
-        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        
-        scrollViewBottomAnchor = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        scrollViewBottomAnchor?.isActive = true
-        
-        scrollView.addSubview(tasksTitleLabel)
-        tasksTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
-        tasksTitleLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16).isActive = true
-        tasksTitleLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16).isActive = true
-        tasksTitleLabel.heightAnchor.constraint(equalToConstant: 48).isActive = true
         
         tasksTitleLabel.text = task?.name
         
@@ -170,11 +124,7 @@ final class TaskController: UIViewController, UITextFieldDelegate {
         textField.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16).isActive = true
         textField.heightAnchor.constraint(equalToConstant: 48).isActive = true
 
-        scrollView.addSubview(subtasksTitleLabel)
-        subtasksTitleLabel.topAnchor.constraint(equalTo: tasksTitleLabel.bottomAnchor, constant: 48).isActive = true
-        subtasksTitleLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16).isActive = true
-        subtasksTitleLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16).isActive = true
-        subtasksTitleLabel.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        subtasksTitleLabel.text = "Subtasks"
 
         scrollView.addSubview(subtasksList.view)
         addChild(subtasksList)
@@ -183,39 +133,7 @@ final class TaskController: UIViewController, UITextFieldDelegate {
         subtasksList.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
         subtasksList.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
         
-        subtasksList.view.heightAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(subtasksList.numItems ?? 0) * subtasksList.itemHeight).isActive = true
-    }
-    
-    private func setupKeyboardObservers() {
-
-        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc private func handleKeyboardWillShow(notification: NSNotification) {
-        
-        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-        guard let keyboardDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
-        
-        scrollViewBottomAnchor?.constant = -keyboardFrame.height
-        
-        var contentInset: UIEdgeInsets = self.scrollView.contentInset
-        contentInset.bottom = 24
-        scrollView.contentInset = contentInset
-        
-        UIView.animate(withDuration: keyboardDuration) {
-            self.view.layoutIfNeeded()
-        }
-    }
-    
-    @objc private func handleKeyboardWillHide(notification: NSNotification) {
-        
-        guard let keyboardDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
-        scrollViewBottomAnchor?.constant = -48
-        UIView.animate(withDuration: keyboardDuration) {
-            self.view.layoutIfNeeded()
-        }
+        setSubtasksListHeightConstraint()
     }
     
     private func setupGesture() {
@@ -229,19 +147,14 @@ final class TaskController: UIViewController, UITextFieldDelegate {
         subtasksList.saveSubtask()
     }
     
-    // MARK: - Text Field Delegate
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        if textField.text == "" {
-            return false
-        } else {
-            textField.resignFirstResponder()
-            return true
-        }
+    private func setSubtasksListHeightConstraint() {
+        subtasksListHeightConstraint = subtasksList.view.heightAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(subtasksList.numItems ?? 0) * subtasksList.itemHeight)
+        subtasksListHeightConstraint?.isActive = true
     }
     
-    @objc func textFieldDidChange(_ textField: UITextField) {
+    // MARK: - Text Field Delegate
+    
+    @objc private func textFieldDidChange(_ textField: UITextField) {
         
         if self.textField.text == "" {
             self.navigationItem.rightBarButtonItem?.isEnabled = false
